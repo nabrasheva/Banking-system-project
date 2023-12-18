b62b8d9cf6f4ea88a68d9403d70c20826cfd4a29
annargeorgieva <annargeorgieva@gmail.com>
2023-12-18 17:51:14 +0200

    Add validations

--- project/src/main/java/com/banking/project/dto/DebitCardDto.java
+++ project/src/main/java/com/banking/project/dto/DebitCardDto.java
@@ -2,10 +2,12 @@ package com.banking.project.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 
@@ -16,9 +18,11 @@ import java.time.LocalDate;
 public class DebitCardDto {
 
     @NotBlank(message = "{number.NotBlank}")
+    @Size(max = 16, message = "{number.Size}")
     private String number;
 
     @NotNull(message = "{expiryDate.NotNull}")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate expiryDate;
 
     @NotNull(message = "{cvv.NotNull}")
