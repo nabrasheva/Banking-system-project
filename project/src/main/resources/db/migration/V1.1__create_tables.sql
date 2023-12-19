333b4e7308d8042932ef53eaeadbfcecda225a9b
Nia Abrasheva <nabrasheva01@gmail.com>
2023-12-19 11:41:41 +0200

    updated the relationship bankuser->account

--- project/src/main/resources/db/migration/V1.1__create_tables.sql
+++ project/src/main/resources/db/migration/V1.1__create_tables.sql
@@ -1,32 +1,33 @@
-CREATE TABLE IF NOT EXISTS public.bank_user (
-                                                id BIGSERIAL PRIMARY KEY,
-                                                username VARCHAR(255) NOT NULL,
-                                                email VARCHAR(255) NOT NULL,
-                                                password VARCHAR(255) NOT NULL,
-                                                country VARCHAR(255) NOT NULL
-);
-
 CREATE TABLE IF NOT EXISTS public.debit_card (
                                                  id BIGSERIAL PRIMARY KEY,
-                                                 number VARCHAR(255) NOT NULL,
+                                                 number VARCHAR(255) UNIQUE NOT NULL,
                                                  expiry_date DATE NOT NULL,
                                                  cvv BIGINT NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS public.account (
                          id BIGSERIAL PRIMARY KEY,
-                         iban VARCHAR(255) NOT NULL,
+                         iban VARCHAR(255) UNIQUE NOT NULL,
                          available_amount NUMERIC(19, 2) NOT NULL,
                          debit_card_id BIGINT,
                          bank_user_id BIGINT,
-                         FOREIGN KEY (debit_card_id) REFERENCES debit_card(id),
-                         FOREIGN KEY (bank_user_id) REFERENCES bank_user(id)
+                         FOREIGN KEY (debit_card_id) REFERENCES debit_card (id)
 );
 
+CREATE TABLE IF NOT EXISTS public.bank_user
+(
+    id         BIGSERIAL PRIMARY KEY,
+    username   VARCHAR(255)        NOT NULL,
+    email      VARCHAR(255) UNIQUE NOT NULL,
+    password   VARCHAR(255)        NOT NULL,
+    country    VARCHAR(255)        NOT NULL,
+    account_id BIGINT,
+    FOREIGN KEY (account_id) REFERENCES account (id)
+);
 
 CREATE TABLE IF NOT EXISTS public.safe (
                       id BIGSERIAL PRIMARY KEY,
-                      name VARCHAR(255) NOT NULL,
+                      name VARCHAR(255) UNIQUE NOT NULL,
                       key VARCHAR(255) NOT NULL,
                       initial_funds NUMERIC(19, 2) NOT NULL,
                       account_id BIGINT,
--- project/src/main/resources/db/migration/V1.2__insert_rows.sql
+++ project/src/main/resources/db/migration/V1.2__insert_rows.sql
@@ -1,19 +1,17 @@
-INSERT INTO public.bank_user (email, username, password, country)
-VALUES ('email1', 'user1', 'password1', 'Country1'),
-       ('email2', 'user2', 'password2', 'Country2'),
-       -- Add more rows as needed
-       ('email3', 'userN', 'passwordN', 'CountryN');
-
 INSERT INTO public.debit_card (number, expiry_date, cvv)
 VALUES ('1234567890123456', '2023-12-31', 123),
        ('9876543210987654', '2024-12-31', 456),
-       -- Add more rows as needed
        ('6543210987654321', '2025-12-31', 789);
 
-INSERT INTO public.account (iban, available_amount, debit_card_id, bank_user_id)
-VALUES ('IBAN123456789', 1000.00, 1, 1),
-       ('IBAN987654321', 500.50, 2, 2),
-       ('IBAN987654325', 999.99, 3, 3);
+INSERT INTO public.account (iban, available_amount, debit_card_id)
+VALUES ('IBAN123456789', 1000.00, 1),
+       ('IBAN987654321', 500.50, 2),
+       ('IBAN987654325', 999.99, 3);
+
+INSERT INTO public.bank_user (email, username, password, country, account_id)
+VALUES ('email1', 'user1', 'password1', 'Country1', 1),
+       ('email2', 'user2', 'password2', 'Country2', 2),
+       ('email3', 'userN', 'passwordN', 'CountryN', 3);
 
 INSERT INTO public.safe (name, key, initial_funds, account_id, creation_date)
 VALUES ('Safe1', 'Key1', 100.00, 1, '2023-12-01'),
